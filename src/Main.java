d706e2b8c025874212d36b7c9602ae2296e592a5
mdrag <m.dragay@yandex.ru>
2023-01-23 15:52:59 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача1");
+        int[] cars = new int[3];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = i + 1;}
+
+        double[] nums = {1.57, 7.654, 9.986};
+
+        boolean[] booleans = {true, false};
+
+
+
     }
 }
